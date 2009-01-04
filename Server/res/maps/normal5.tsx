68a05610c4d900a697612acf99d62d907b4a7f10
tmk69 <tmk69@hotmail.com>
2009-01-04 01:16:24 +0000

    Map fixes.. DAMN NO DOUBLE COLLISIONS LAYERS!

--- Server/res/maps/normal5.tsx
+++ Server/res/maps/normal5.tsx
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
  <tileset name="1" firstgid="1" tilewidth="16" tileheight="16">
   <image source="normal5.png" trans="000000"/>
-  <tile id="350">
+  <tile id="347">
    <properties>
-    <property name="anim" value="347, 350, 347, 350"/>
+    <property name="anim" value="350, 347, 350, 347, 350"/>
     <property name="anim_speed" value="10"/>
    </properties>
   </tile>
